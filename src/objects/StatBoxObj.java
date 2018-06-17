0396dbf016ca901cf4c0457d9fc6c03c786ea400
eliasr123 <te16.elias.renman@umea.nti.se>
2018-06-17 19:16:55 +0200

    updated logic

--- src/objects/StatBoxObj.java
+++ src/objects/StatBoxObj.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class StatBoxObj {
-
-    public ArrayList<String> options;
-
-    public void setInitialOptions() {
-        String[] initialOptions = {null,"+3","+2","+1","+1","+1","+1","+0","-1"};
-        this.options = new ArrayList<>(Arrays.asList(initialOptions));
+    private String[] initialOptions = {" ","+2","+2","+1","+0","+0","-1","-2"};
+    public ArrayList<String> options = new ArrayList<>(Arrays.asList(initialOptions));
+    public String currentSelected = null;
+    public void removeOption(String toBeRemoved) {
+        options.remove(toBeRemoved);
     }
+    public void addOption(String toBeAdded) {
+        options.add(toBeAdded);
+    }
+
 
 
 }
